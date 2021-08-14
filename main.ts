8fe905d980334e107dc0ff5d0cea2eee105d8571
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-08-13 18:18:24 -0700

    Minor updates

--- main.ts
+++ main.ts
@@ -17,51 +17,59 @@ export default class MyPlugin extends Plugin {
 
         const isHeading = isHeadingRegEx.test(line);
 
-        menu.addItem((item) => {
-          const onClick = (isEmbed: boolean) => {
-            // Copy heading
-            if (isHeading) {
-              return navigator.clipboard.writeText(
-                `${isEmbed ? "!" : ""}[[${basename}#${line.replace(
-                  /^#+\s/,
-                  ""
-                )}]]`
-              );
-            }
-
-            const match = line.match(blockIdRegEx);
-
-            // Copy existing block id
-            if (match) {
-              return navigator.clipboard.writeText(
-                `${isEmbed ? "!" : ""}[[${basename}#^${match[1]}]]`
-              );
-            }
-
-            // Add a block id
-            const end: EditorPosition = {
-              line: cursor.line,
-              ch: line.length,
-            };
-
-            const id = generateId();
-
-            editor.replaceRange(` ^${id}`, end);
-            navigator.clipboard.writeText(
-              `${isEmbed ? "!" : ""}[[${basename}#^${id}]]`
+        const onClick = (isEmbed: boolean) => {
+          // Copy heading
+          if (isHeading) {
+            return navigator.clipboard.writeText(
+              `${isEmbed ? "!" : ""}[[${basename}#${line.replace(
+                /^#+\s/,
+                ""
+              )}]]`
             );
+          }
+
+          const match = line.match(blockIdRegEx);
+
+          // Copy existing block id
+          if (match) {
+            return navigator.clipboard.writeText(
+              `${isEmbed ? "!" : ""}[[${basename}#^${match[1]}]]`
+            );
+          }
+
+          // Add a block id
+          const end: EditorPosition = {
+            line: cursor.line,
+            ch: line.length,
           };
 
+          const id = generateId();
+          const isBlockquote = /^\s*>+./.test(line);
+
+          // TODO: better handle blockquotes and codeblocks
+
+          editor.replaceRange(
+            `${isBlockquote ? "\n\n" : " "}^${id}${isBlockquote ? "\n" : ""}`,
+            end
+          );
+          navigator.clipboard.writeText(
+            `${isEmbed ? "!" : ""}[[${basename}#^${id}]]`
+          );
+        };
+
+        menu.addItem((item) => {
           item
             .setTitle(isHeading ? "Copy link to heading" : "Copy link to block")
             .setIcon("links-coming-in")
             .onClick(() => onClick(false));
+        });
 
+        menu.addItem((item) => {
           item
             .setTitle(
               isHeading
-                ? "Copy embedded link to heading"
-                : "Copy embedded link to block"
+                ? "Copy heading embed"
+                : "Copy link embed"
             )
             .setIcon("links-coming-in")
             .onClick(() => onClick(true));
