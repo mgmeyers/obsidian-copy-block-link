a2db5faca90600249a1f0cc0049afe0b65081ef5
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-08-13 18:10:38 -0700

    Add embed and non-embed options

--- main.ts
+++ main.ts
@@ -18,37 +18,53 @@ export default class MyPlugin extends Plugin {
         const isHeading = isHeadingRegEx.test(line);
 
         menu.addItem((item) => {
+          const onClick = (isEmbed: boolean) => {
+            // Copy heading
+            if (isHeading) {
+              return navigator.clipboard.writeText(
+                `${isEmbed ? "!" : ""}[[${basename}#${line.replace(
+                  /^#+\s/,
+                  ""
+                )}]]`
+              );
+            }
+
+            const match = line.match(blockIdRegEx);
+
+            // Copy existing block id
+            if (match) {
+              return navigator.clipboard.writeText(
+                `${isEmbed ? "!" : ""}[[${basename}#^${match[1]}]]`
+              );
+            }
+
+            // Add a block id
+            const end: EditorPosition = {
+              line: cursor.line,
+              ch: line.length,
+            };
+
+            const id = generateId();
+
+            editor.replaceRange(` ^${id}`, end);
+            navigator.clipboard.writeText(
+              `${isEmbed ? "!" : ""}[[${basename}#^${id}]]`
+            );
+          };
+
           item
             .setTitle(isHeading ? "Copy link to heading" : "Copy link to block")
             .setIcon("links-coming-in")
-            .onClick(() => {
-              // Copy heading
-              if (isHeading) {
-                return navigator.clipboard.writeText(
-                  `![[${basename}#${line.replace(/^#+\s/, "")}]]`
-                );
-              }
+            .onClick(() => onClick(false));
 
-              const match = line.match(blockIdRegEx);
-
-              // Copy existing block id
-              if (match) {
-                return navigator.clipboard.writeText(
-                  `![[${basename}#^${match[1]}]]`
-                );
-              }
-
-              // Add a block id
-              const end: EditorPosition = {
-                line: cursor.line,
-                ch: line.length,
-              };
-
-              const id = generateId();
-
-              editor.replaceRange(` ^${id}`, end);
-              navigator.clipboard.writeText(`![[${basename}#^${id}]]`);
-            });
+          item
+            .setTitle(
+              isHeading
+                ? "Copy embedded link to heading"
+                : "Copy embedded link to block"
+            )
+            .setIcon("links-coming-in")
+            .onClick(() => onClick(true));
         });
       })
     );
