79b0e5ed415339a18b27b3676e6edd164c0c647a
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-08-31 13:09:38 -0700

    Add commands to copy links and embeds

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import { EditorPosition, ListItemCache, Plugin, SectionCache } from "obsidian";
+import {
+  Editor,
+  EditorPosition,
+  HeadingCache,
+  ListItemCache,
+  MarkdownView,
+  Plugin,
+  SectionCache,
+  TFile,
+} from "obsidian";
 
 function generateId(): string {
   return Math.random().toString(36).substr(2, 6);
@@ -20,85 +29,23 @@ export default class MyPlugin extends Plugin {
   async onload() {
     this.registerEvent(
       this.app.workspace.on("editor-menu", (menu, editor, view) => {
-        const cursor = editor.getCursor("to");
-        const fileCache = this.app.metadataCache.getFileCache(view.file);
+        const block = this.getBlock(editor, view.file);
 
-        let blockCache: ListItemCache | SectionCache = (
-          fileCache?.sections || []
-        ).find((section) => {
-          return (
-            section.position.start.line <= cursor.line &&
-            section.position.end.line >= cursor.line
-          );
-        });
+        if (!block) return;
 
-        if (!blockCache) return;
-
-        if (blockCache.type === "list") {
-          blockCache = (fileCache?.listItems || []).find((item) => {
-            return (
-              item.position.start.line <= cursor.line &&
-              item.position.end.line >= cursor.line
-            );
-          });
-        }
-
-        const isHeading = (blockCache as any).type === "heading";
+        const isHeading = !!(block as any).heading;
 
         const onClick = (isEmbed: boolean) => {
-          // Copy heading
           if (isHeading) {
-            const heading = fileCache.headings.find((heading) => {
-              return (
-                heading.position.start.line === blockCache.position.start.line
-              );
-            });
-
-            return (
-              heading &&
-              navigator.clipboard.writeText(
-                `${
-                  isEmbed ? "!" : ""
-                }${this.app.fileManager.generateMarkdownLink(
-                  view.file,
-                  "",
-                  "#" + heading.heading
-                )}`
-              )
-            );
-          }
-
-          const blockId = blockCache.id;
-
-          // Copy existing block id
-          if (blockId) {
-            return navigator.clipboard.writeText(
-              `${isEmbed ? "!" : ""}${this.app.fileManager.generateMarkdownLink(
-                view.file,
-                "",
-                "#^" + blockId
-              )}`
-            );
-          }
-
-          // Add a block id
-          const sectionEnd = blockCache.position.end;
-          const end: EditorPosition = {
-            ch: sectionEnd.col,
-            line: sectionEnd.line,
-          };
-
-          const id = generateId();
-          const spacer = shouldInsertAfter(blockCache) ? "\n\n" : " ";
-
-          editor.replaceRange(`${spacer}^${id}`, end);
-          navigator.clipboard.writeText(
-            `${isEmbed ? "!" : ""}${this.app.fileManager.generateMarkdownLink(
+            this.handleHeading(view.file, block as HeadingCache, isEmbed);
+          } else {
+            this.handleBlock(
               view.file,
-              "",
-              "#^" + id
-            )}`
-          );
+              editor,
+              block as SectionCache | ListItemCache,
+              isEmbed
+            );
+          }
         };
 
         menu.addItem((item) => {
@@ -115,6 +62,123 @@ export default class MyPlugin extends Plugin {
             .onClick(() => onClick(true));
         });
       })
+    );
+
+    this.addCommand({
+      id: 'copy-link-to-block',
+      name: "Copy link to current block or heading",
+      editorCheckCallback: (isChecking, editor, view) => {
+        return this.handleCommand(isChecking, editor, view, false)
+      },
+    })
+
+    this.addCommand({
+      id: 'copy-embed-to-block',
+      name: "Copy embed to current block or heading",
+      editorCheckCallback: (isChecking, editor, view) => {
+        return this.handleCommand(isChecking, editor, view, true)
+      },
+    })
+  }
+
+  handleCommand(isChecking: boolean, editor: Editor, view: MarkdownView, isEmbed: boolean) {
+      if (isChecking) {
+        return !!this.getBlock(editor, view.file);
+      }
+      
+      const block = this.getBlock(editor, view.file);
+
+      if (!block) return;
+
+      const isHeading = !!(block as any).heading;
+
+      if (isHeading) {
+        this.handleHeading(view.file, block as HeadingCache, isEmbed);
+      } else {
+        this.handleBlock(
+          view.file,
+          editor,
+          block as SectionCache | ListItemCache,
+          isEmbed
+        );
+      }
+    }
+
+  getBlock(editor: Editor, file: TFile) {
+    const cursor = editor.getCursor("to");
+    const fileCache = this.app.metadataCache.getFileCache(file);
+
+    let block: ListItemCache | HeadingCache | SectionCache = (
+      fileCache?.sections || []
+    ).find((section) => {
+      return (
+        section.position.start.line <= cursor.line &&
+        section.position.end.line >= cursor.line
+      );
+    });
+
+    if (block?.type === "list") {
+      block = (fileCache?.listItems || []).find((item) => {
+        return (
+          item.position.start.line <= cursor.line &&
+          item.position.end.line >= cursor.line
+        );
+      });
+    } else if (block?.type === "heading") {
+      block = fileCache.headings.find((heading) => {
+        return heading.position.start.line === block.position.start.line;
+      });
+    }
+
+    return block
+  }
+
+  handleHeading(file: TFile, block: HeadingCache, isEmbed: boolean) {
+    navigator.clipboard.writeText(
+      `${isEmbed ? "!" : ""}${this.app.fileManager.generateMarkdownLink(
+        file,
+        "",
+        "#" + block.heading
+      )}`
+    );
+  }
+
+  handleBlock(
+    file: TFile,
+    editor: Editor,
+    block: ListItemCache | SectionCache,
+    isEmbed: boolean
+  ) {
+    const blockId = block.id;
+
+    // Copy existing block id
+    if (blockId) {
+      return navigator.clipboard.writeText(
+        `${isEmbed ? "!" : ""}${this.app.fileManager.generateMarkdownLink(
+          file,
+          "",
+          "#^" + blockId
+        )}`
+      );
+    }
+
+    // Add a block id
+    const sectionEnd = block.position.end;
+    const end: EditorPosition = {
+      ch: sectionEnd.col,
+      line: sectionEnd.line,
+    };
+
+    const id = generateId();
+    const spacer = shouldInsertAfter(block) ? "\n\n" : " ";
+
+    editor.replaceRange(`${spacer}^${id}`, end);
+    navigator.clipboard.writeText(
+      `${isEmbed ? "!" : ""}${this.app.fileManager.generateMarkdownLink(
+        file,
+        "",
+        "#^" + id
+      )}`
     );
   }
 }
